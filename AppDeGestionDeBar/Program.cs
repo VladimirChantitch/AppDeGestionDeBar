f2e4e57638c5d2ca4256a1fd4f0f66fd3ebc46d9
Vladimir Chantitch <chantitch.vladimir@gmail.com>
2023-01-16 14:43:26 +0100

    Ajoutez des fichiers projet.

---
+++ AppDeGestionDeBar/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace bar
+{
+    class GestionDeBar
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bingo!");
+            Console.ReadKey();
+        }
+    }
+}
