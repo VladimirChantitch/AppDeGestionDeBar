b462f3f050382ec7776aea35ca72d2f8662ef6a3
felixlaterrot <felixlaterrot@gmail.com>
2023-01-17 10:26:01 +0100

    j

--- GestionBarInterface/Form1.Designer.cs
+++ GestionBarInterface/Form1.Designer.cs
@@ -161,7 +161,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(982, 495);
             this.Controls.Add(this.gpbCreationCommande);
             this.Name = "Form1";
             this.Text = "Form1";
